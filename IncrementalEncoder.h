0c0ecd432384e64916ebcea8c9f7de9e84247421
AZAIS Corentin <haellsigh@gmail.com>
2019-11-21 14:17:17 +0100

    Ajout de l'encodeur

---
+++ IncrementalEncoder.h
@@ -0,0 +1,13 @@
+#ifndef PENDULE_INCREMENTALENCODER_H
+#define PENDULE_INCREMENTALENCODER_H
+
+class IncrementalEncoder
+{
+public:
+    IncrementalEncoder(/* args */);
+    ~IncrementalEncoder();
+};
+
+
+
+#endif // PENDULE_INCREMENTALENCODER_H
--- MIX5A_PenduleInverse.ino
+++ MIX5A_PenduleInverse.ino
@@ -1,17 +1,76 @@
 #include "configuration.h"
 #include "SensorFusion.h"
 
-SensorFusion sensors(pinLeftSensor, pinRightSensor, nBits);
+#include <stdint.h>
+
+//SensorFusion sensors(pinLeftSensor, pinRightSensor, nBits);
+
+int32_t currentStep = 0;
+
+// Unknown initial state
+uint8_t EncoderAState, EncoderBState;
+
+void stepAChange()
+{
+  EncoderAState = ((EncoderAState == LOW) ? HIGH : LOW);
+
+  if (EncoderAState == HIGH) {
+    if (EncoderBState == LOW)
+      currentStep++;
+    else
+      currentStep--;
+  } else {
+    if (EncoderBState == HIGH)
+      currentStep++;
+    else
+      currentStep--;
+  }
+}
+
+void stepBChange()
+{
+  EncoderBState = ((EncoderBState == LOW) ? HIGH : LOW);
+
+  if (EncoderBState == HIGH) {
+    if (EncoderAState == HIGH)
+      currentStep++;
+    else
+      currentStep--;
+  } else {
+    if (EncoderAState == LOW)
+      currentStep++;
+    else
+      currentStep--;
+  }
+}
 
 void setup()
 {
-  sensors.setSensorCoefficients(coefsLeftSensor, coefsRightSensor);
+  //sensors.setSensorCoefficients(coefsLeftSensor, coefsRightSensor);
   Serial.begin(115200);
+
+  pinMode(49, INPUT_PULLUP);
+  pinMode(51, INPUT_PULLUP);
+
+  delay(10);
+
+  noInterrupts();
+  attachInterrupt(digitalPinToInterrupt(49), stepAChange, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(51), stepBChange, CHANGE);
+  EncoderAState = digitalRead(49);
+  EncoderBState = digitalRead(51);
+  interrupts();
 }
 
 void loop()
 {
-  float distance = sensors.update();
+  //float distance = sensors.update();
 
-  delay(50);
+  int stepn;
+  noInterrupts();
+  stepn = currentStep % 2048;
+  interrupts();
+
+  Serial.println(360. * stepn / 4096.);
+  delay(10);
 }
